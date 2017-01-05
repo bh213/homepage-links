a7e33f2f9a2d4873e86cf1a43c426f339748dd58
Gorazd Breskvar <gorazd.breskvar@whiletrue.com>
2017-01-05 15:33:36 +0100

    updates

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ index.config = config;
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/public/stylesheets/')); // redirect CSS bootstrap
+app.use('/awesome', express.static(__dirname + '/node_modules/font-awesome'));
 
 app.use("/", index);
 
